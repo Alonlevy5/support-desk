6b357b5174ed72c962a7bc6f7bff073d70d75dde
alon <alonlevy5@gmail.com>
2023-02-02 20:38:16 +0200

    added createTIcket UI

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate, navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { useSelector, useDispatch } from 'react-redux';
